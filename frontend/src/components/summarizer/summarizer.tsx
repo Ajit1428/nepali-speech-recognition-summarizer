ace8374fbeaf77d79ded9c05a7c49d9e8ce89991
Ajit Thapa <ajitthapa248@gmail.com>
2024-11-20 11:27:59 +0545

    (fix): Fixed the styles in summarizer

--- frontend/src/components/summarizer/summarizer.tsx
+++ frontend/src/components/summarizer/summarizer.tsx
@@ -34,13 +34,13 @@ export default function SummarizerComponent() {
         <h1 className="text-[1.7rem] font-[800] capitalize text-blue-500 md:text-[2.5rem] xl:text-[3rem]">
           Summarize your audio files
         </h1>
-        <span className="text-xs md:text-sm xl:text-lg">
+        <span className="text-xs md:text-sm xl:text-[16px]">
           Upload your audio file of any extension. e.g mp3, m4a, wav.
         </span>
       </div>
       <div
         className={cn(
-          "mb-4 flex h-[30rem] w-[30rem] flex-col items-center justify-center gap-6 bg-gray-200/40 p-4 md:mb-0 md:w-[45rem] lg:w-[60rem] xl:w-[80rem]",
+          "mb-4 flex h-[30rem] w-[25rem] flex-col items-center justify-center gap-6 bg-gray-200/40 p-4 md:mb-0 md:w-[45rem] lg:w-[60rem] xl:w-[80rem]",
           responseData && "h-auto",
         )}
       >
@@ -50,25 +50,25 @@ export default function SummarizerComponent() {
               className="inline-block size-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-blue-500 motion-reduce:animate-[spin_1.5s_linear_infinite] "
               role="status"
             />
-            <span className="text-sm font-semibold">
+            <span className="text-xs font-semibold md:text-sm">
               Transcripting and summarizing your audio file, please wait...
             </span>
           </div>
         ) : responseData ? (
           <div className="flex flex-col items-start justify-start gap-10 p-10">
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-4">
               <h1 className="w-fit border-b-4 border-double border-black text-xl font-bold">
                 Transcript
               </h1>
-              <span className="text-lg md:text-xl">
+              <span className="text-lg xl:text-xl">
                 {responseData?.transcript}
               </span>
             </div>
-            <div className="flex flex-col gap-2">
+            <div className="flex flex-col gap-4">
               <h1 className="w-fit border-b-4 border-double border-black text-xl font-bold">
                 Summary
               </h1>
-              <span className="text-lg md:text-xl">
+              <span className="text-lg xl:text-xl">
                 {responseData?.summary}
               </span>
             </div>
